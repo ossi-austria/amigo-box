31c7f7435d0bcdffe12c40a91b7392666c112f35
Anamaria Skočić <ana.skocic@gmail.com>
2021-05-21 16:18:00 +0000

    Feature #59: Jitsi Fragment draft
    - add draft for Jitsi Fragment
    - connect with activity
    - handle permissions

---
+++ app-amigobox/src/main/java/org/ossiaustria/amigobox/ui/jitsi/JitsiViewModel.kt
@@ -0,0 +1,10 @@
+package org.ossiaustria.amigobox.ui.jitsi
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.ossiaustria.lib.domain.repositories.AlbumRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class JitsiViewModel  @Inject constructor() : ViewModel() {
+}
