d011b341552d04357222ee0f40fab66a284a01ab
Anamaria Skočić <ana.skocic@gmail.com>
2021-10-10 07:58:38 +0000

    Feature#179 - Add Personenansicht

--- app-amigobox/src/main/java/org/ossiaustria/amigobox/contacts/PersonDetailFragment.kt
+++ app-amigobox/src/main/java/org/ossiaustria/amigobox/contacts/PersonDetailFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -20,15 +23,17 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import dagger.hilt.android.AndroidEntryPoint
 import org.ossiaustria.amigobox.Navigator
 import org.ossiaustria.amigobox.R
+import org.ossiaustria.amigobox.ui.UIConstants
 import org.ossiaustria.amigobox.ui.commons.AmigoStyle
-import org.ossiaustria.amigobox.ui.commons.MaterialButton
-import org.ossiaustria.amigobox.ui.commons.NetworkImage
+import org.ossiaustria.amigobox.ui.commons.IconButtonSmall
+import org.ossiaustria.amigobox.ui.commons.MaterialThemeLight
+import org.ossiaustria.amigobox.ui.commons.TextAndIconButton
+import org.ossiaustria.amigobox.ui.commons.profileImage
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -44,7 +49,7 @@ class PersonDetailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = ComposeView(requireContext()).apply {
-        setContent { PersonDetailScreen(globalState) }
+        setContent { PersonDetailScreen(globalState, ::startCall, ::toHome) }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -54,51 +59,119 @@ class PersonDetailFragment : Fragment() {
         // init stuff
     }
 
-    @Composable
-    fun PersonDetailScreen(state: GlobalStateViewModel) {
-        val personState by state.selectedPerson.observeAsState()
-        if (personState != null) {
-            MaterialTheme {
-                PersonDetailFragmentComposable(personState!!.name, "https://picsum.photos/300/300")
+    fun startCall() {
+        navigator.toCallPerson(globalState.selectedPerson.value!!)
+    }
+
+    fun toHome() {
+        navigator.toHome()
+    }
+}
+
+@Composable
+fun PersonDetailScreen(
+    state: GlobalStateViewModel,
+    startCall: () -> Unit,
+    toHome: () -> Unit,
+) {
+    val personState by state.selectedPerson.observeAsState()
+    if (personState != null) {
+        MaterialThemeLight {
+            Surface(color = MaterialTheme.colors.background) {
+                PersonDetailFragmentComposable(
+                    personState!!.name,
+                    "https://picsum.photos/300/300",
+                    startCall,
+                    toHome
+                )
             }
         }
     }
+}
 
-    @Composable
-    fun PersonDetailFragmentComposable(name: String, pictureUrl: String) {
-        MaterialTheme {
-            Row(Modifier.padding(16.dp)) {
-                NetworkImage(
-                    url = pictureUrl,
-                    modifier = Modifier
-                        .fillMaxWidth(0.5F)
-                        .fillMaxHeight(),
-                    contentScale = ContentScale.Crop
+@Composable
+fun PersonDetailFragmentComposable(
+    name: String,
+    pictureUrl: String,
+    startCall: () -> Unit,
+    toHome: () -> Unit,
+) {
+    Column(Modifier.fillMaxSize()) {
+        Row(
+            Modifier
+                .padding(UIConstants.PersonDetailFragment.TOP_BAR_PADDING)
+                .fillMaxWidth()
+                .height(UIConstants.PersonDetailFragment.TOP_BAR_HEIGHT),
+            horizontalArrangement = Arrangement.End
+        ) {
+            IconButtonSmall(
+                resourceId = R.drawable.ic_home_icon,
+                backgroundColor = MaterialTheme.colors.background,
+                fillColor = MaterialTheme.colors.surface,
+            ) {
+                toHome()
+            }
+            IconButtonSmall(
+                resourceId = R.drawable.ic_help_icon,
+                backgroundColor = MaterialTheme.colors.background,
+                fillColor = MaterialTheme.colors.primary,
+            ) {
+                //TODO: Add help screens
+            }
+        }
+        Row(
+            Modifier
+                .padding(UIConstants.PersonDetailFragment.SEC_ROW_PADDING)
+        ) {
+            /*NetworkImage(
+                url = pictureUrl,
+                modifier = Modifier
+                    .fillMaxWidth(0.5F),
+                contentScale = ContentScale.Crop
+            )
+             */
+            profileImage(
+                url = pictureUrl,
+                modifier = Modifier,
+                contentScale = ContentScale.Crop,
+            )
+
+            Column(modifier = Modifier.padding(UIConstants.PersonDetailFragment.COLUMN_PADDING)) {
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h2
                 )
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = name,
-                        style = MaterialTheme.typography.h2
-                    )
-                    Spacer(modifier = Modifier.padding(AmigoStyle.Dim.C))
-                    MaterialButton(
-                        onClick = { startCall() },
-                        modifier = Modifier,
-                        text = stringResource(R.string.person_detail_call_button, name)
-                    )
-                    Spacer(modifier = Modifier.padding(AmigoStyle.Dim.C))
-                    MaterialButton(
-                        onClick = { startCall() },
-                        modifier = Modifier,
-                        text = stringResource(R.string.person_detail_albums_button, name)
-                    )
+                Spacer(modifier = Modifier.padding(AmigoStyle.Dim.C))
+                TextAndIconButton(
+                    R.drawable.ic_phone_call,
+                    stringResource(R.string.person_detail_call_button, name),
+                    MaterialTheme.colors.secondary,
+                    MaterialTheme.colors.onSecondary,
+                    false,
+                    true,
+                    UIConstants.PersonDetailFragment.CALL_BUTTON_WIDTH
+                ) { startCall() }
+                Spacer(modifier = Modifier.padding(AmigoStyle.Dim.C))
+                TextAndIconButton(
+                    R.drawable.ic_image_light,
+                    stringResource(R.string.person_detail_albums_button, name),
+                    MaterialTheme.colors.secondary,
+                    MaterialTheme.colors.onSecondary,
+                    false,
+                    true,
+                    UIConstants.PersonDetailFragment.ALBUM_BUTTON_WIDTH
+                ) {
+                    //TODO: missing fragment
                 }
             }
         }
     }
+}
 
-    private fun startCall() {
-        navigator.toCallPerson(globalState.selectedPerson.value!!)
-    }
-
+@Preview
+@Composable
+fun PersonDetailFragmentPreview() {
+    PersonDetailFragmentComposable(
+        "name", "pictureUrl", {}, {}
+    )
 }
