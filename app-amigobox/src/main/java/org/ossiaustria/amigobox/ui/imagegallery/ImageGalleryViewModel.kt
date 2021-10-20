cfd921d045874b264ee8f7dd66d88d98d4f629ae
Gusztáv Ottrubay <g.ottrubay@gmail.com>
2021-10-20 17:26:33 +0000

    Feature#182 - Add new (better) branch navigation gallery

--- app-amigobox/src/main/java/org/ossiaustria/amigobox/ui/imagegallery/ImageGalleryViewModel.kt
+++ app-amigobox/src/main/java/org/ossiaustria/amigobox/ui/imagegallery/ImageGalleryViewModel.kt
@@ -1,5 +1,6 @@
 package org.ossiaustria.amigobox.ui.imagegallery
 
+import android.os.CountDownTimer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.CoroutineDispatcher
@@ -10,6 +11,79 @@ class ImageGalleryViewModel(
     ioDispatcher: CoroutineDispatcher
 ) : BoxViewModel(ioDispatcher) {
 
-    private val _currentAlbum = MutableLiveData<Album>()
-    val currentAlbum: LiveData<Album> = _currentAlbum
+    private val _currentAlbum = MutableLiveData<Album?>()
+    val currentAlbum: MutableLiveData<Album?> = _currentAlbum
+
+    private val _navigationState = MutableLiveData(GalleryNavState.PLAY)
+    val navigationState: MutableLiveData<GalleryNavState> = _navigationState
+
+    private val _autoState = MutableLiveData(AutoState.CHANGE)
+    val autoState: MutableLiveData<AutoState> = _autoState
+
+    private val _currentGalleryIndex = MutableLiveData(0)
+    val currentGalleryIndex: MutableLiveData<Int> = _currentGalleryIndex
+
+    // Timer variables
+    private var countDownTimer: CountDownTimer? = null
+
+    private val _time = MutableLiveData(CountdownFormat.TIME_COUNTDOWN.formatTime())
+    val time: LiveData<String> = _time
+
+    // not yet needed but can be used to track progress
+    private val _progress = MutableLiveData(1.00F)
+    val progress: LiveData<Float> = _progress
+
+    private val _isPlaying = MutableLiveData(false)
+    val isPlaying: LiveData<Boolean> = _isPlaying
+
+    // set states with following methods
+    fun setNavigationState(navState: GalleryNavState) {
+        _navigationState.value = navState
+    }
+
+    fun setGalleryIndex(currentGalleryIndex: Int) {
+        _currentGalleryIndex.value = currentGalleryIndex
+    }
+
+    fun setAutoState(autoState: AutoState) {
+        _autoState.value = autoState
+    }
+
+    fun pauseTimer() {
+        countDownTimer?.cancel()
+        handleTimerValues(false, CountdownFormat.TIME_COUNTDOWN.formatTime(), 1.0F)
+    }
+
+    fun startTimer() {
+
+        _isPlaying.value = true
+        countDownTimer = object : CountDownTimer(CountdownFormat.TIME_COUNTDOWN, 1000) {
+
+            override fun onTick(millisRemaining: Long) {
+                val progressValue = millisRemaining.toFloat() / CountdownFormat.TIME_COUNTDOWN
+                handleTimerValues(true, millisRemaining.formatTime(), progressValue)
+            }
+
+            override fun onFinish() {
+                startTimer()
+
+            }
+        }.start()
+    }
+
+    fun cancelTimer() {
+        countDownTimer?.cancel()
+    }
+
+    private fun handleTimerValues(
+        isPlaying: Boolean,
+        text: String,
+        progress: Float
+    ) {
+        _isPlaying.value = isPlaying
+        _time.value = text
+        _progress.value = progress
+    }
 }
+
+
